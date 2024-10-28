963f3c0c92d205f85ab0b68dec82452137fda536
Tgodwin94 <tyler.godwin94@gmail.com>
2024-10-28 03:34:59 +0000

    Finished

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
+import "/workspaces/TylerGodwin-ContactList_With_React_And_Context/src/styles/home.css";
 
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
 		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
+			Made with <span className="code"> <i className="fa-solid fa-code"></i> </span> by <span className="me"> Tyler </span>, <span className="react"> React <i className="fa-brands fa-react"></i> </span>, and <span className="context"> Context </span>
 		</p>
 	</footer>
 );
